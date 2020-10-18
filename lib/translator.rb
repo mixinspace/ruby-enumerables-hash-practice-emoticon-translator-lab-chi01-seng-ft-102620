afe0ff287e92b87d8a1b71f0da14804b33ec5376
Islam Madkaur <islammadkaur@gmail.com>
2020-10-18 18:49:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,6 +1,7 @@
 require "yaml"
 
-
+def load_library
+  # code goes here
 def load_library(file_path)
   emoticons = YAML.load_file(file_path)
 
@@ -29,5 +30,4 @@ def get_english_meaning
 def get_english_meaning(file_path, emoticon)
   result = load_library(file_path)['get_meaning'][emoticon]
   result ? result : "Sorry, that emoticon was not found"
-end
-end
+end 
