810ef0baa73e47a3a0f89c5053149cc59af66dd6
Islam Madkaur <islammadkaur@gmail.com>
2020-10-18 19:00:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -32,7 +32,7 @@ def get_japanese_emoticon(file_path, emoticon)
 end
 
 def get_english_meaning(file_path, emoticon)
-  result = load_library(file_path)['get_emoticon'][emoticon]
+  result = load_library(file_path)['get_meaning'][emoticon]
   result ? result : "Sorry, that emoticon was not found"
   library = load_library(file_path)
   emoticon = library.keys.find do |key|
